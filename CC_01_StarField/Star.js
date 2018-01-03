73f6aaf773ce0e40c1554049ee45a50332a1e6bb
micuat <micuat@gmail.com>
2018-01-03 10:38:07 -0500

    01 instance mode

---
+++ CC_01_StarField/Star.js
@@ -0,0 +1,36 @@
+function Star(sketch) {
+  this.x = sketch.random(-sketch.width, sketch.width);
+  this.y = sketch.random(-sketch.height, sketch.height);
+  this.z = sketch.random(sketch.width);
+  this.pz = this.z;
+
+  this.update = function(speed) {
+    this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = sketch.width;
+      this.x = sketch.random(-sketch.width, sketch.width);
+      this.y = sketch.random(-sketch.height, sketch.height);
+      this.pz = this.z;
+    }
+  }
+
+  this.show = function() {
+    sketch.fill(255);
+    sketch.noStroke();
+
+    var sx = sketch.map(this.x / this.z, 0, 1, 0, sketch.width);
+    var sy = sketch.map(this.y / this.z, 0, 1, 0, sketch.height);
+
+    var r = sketch.map(this.z, 0, sketch.width, 16, 0);
+    sketch.ellipse(sx, sy, r, r);
+
+    var px = sketch.map(this.x / this.pz, 0, 1, 0, sketch.width);
+    var py = sketch.map(this.y / this.pz, 0, 1, 0, sketch.height);
+
+    this.pz = this.z;
+
+    sketch.stroke(255);
+    sketch.line(px, py, sx, sy);
+
+  }
+}
---
+++ CC_01_StarField/sketch.js
@@ -0,0 +1,30 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/17WoOqgXsRM
+
+var s = function( sketch ) {
+
+  var stars = [];
+
+  var speed;
+
+  sketch.setup = function setup() {
+    sketch.createCanvas(600, 600);
+    for (var i = 0; i < 800; i++) {
+      stars[i] = new Star(sketch);
+    }
+  }
+
+  sketch.draw = function() {
+    speed = sketch.map(sketch.mouseX, 0, sketch.width, 0, 50);
+    sketch.background(0);
+    sketch.translate(sketch.width / 2, sketch.height / 2);
+    for (var i = 0; i < stars.length; i++) {
+      stars[i].update(speed);
+      stars[i].show();
+    }
+  };
+};
+
+var myp5 = new p5(s);
