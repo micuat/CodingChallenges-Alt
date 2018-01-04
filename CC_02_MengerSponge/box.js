2f71e3b477079b75deed29085bda5db9a9d650df
micuat <micuat@gmail.com>
2018-01-04 11:02:09 -0500

    02 instance mode

---
+++ CC_02_MengerSponge/box.js
@@ -0,0 +1,37 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for this video: https://youtu.be/LG8ZK-rRkXo
+
+function Box(sketch, x, y, z, r) {
+  this.pos = sketch.createVector(x, y, z);
+  this.r = r;
+
+  this.generate = function () {
+    var boxes = [];
+    for (var x = -1; x < 2; x++) {
+      for (var y = -1; y < 2; y++) {
+        for (var z = -1; z < 2; z++) {
+          var sum = sketch.abs(x) + sketch.abs(y) + sketch.abs(z);
+          var newR = this.r / 3;
+          if (sum > 1) {
+            var b = new Box(sketch, this.pos.x + x * newR, this.pos.y + y * newR, this.pos.z + z * newR, newR);
+            boxes.push(b);
+          }
+        }
+      }
+    }
+    return boxes;
+  }
+
+  this.show = function () {
+    sketch.push();
+    sketch.translate(this.pos.x, this.pos.y, this.pos.z);
+    sketch.stroke(255);
+    sketch.noStroke();
+    sketch.noFill();
+    //fill(255);
+    sketch.box(this.r);
+    sketch.pop();
+  }
+}
---
+++ CC_02_MengerSponge/sketch.js
@@ -0,0 +1,48 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for this video: https://youtu.be/LG8ZK-rRkXo
+
+// instance mode by Naoto Hieda
+
+
+var s = function (sketch) {
+
+  var a = 0;
+
+  var sponge = [];
+
+  sketch.setup = function () {
+    sketch.createCanvas(400, 400, sketch.WEBGL);
+
+    // An array of Box objects
+    // Star with one
+    var b = new Box(sketch, 0, 0, 0, 200);
+    sponge.push(b);
+  }
+
+  sketch.mousePressed = function () {
+    // Generate the next set of boxes
+    var next = [];
+    for (var i = 0; i < sponge.length; i++) {
+      var b = sponge[i];
+      var newBoxes = b.generate();
+      next = next.concat(newBoxes);
+    }
+    sponge = next;
+  }
+
+  sketch.draw = function () {
+    sketch.background(51);
+    sketch.rotateX(a);
+    sketch.rotateY(a * 0.4);
+    sketch.rotateZ(a * 0.1);
+    // Show what you've got!
+    for (var i = 0; i < sponge.length; i++) {
+      sponge[i].show();
+    }
+    a += 0.01;
+  }
+};
+
+var myp5 = new p5(s);
