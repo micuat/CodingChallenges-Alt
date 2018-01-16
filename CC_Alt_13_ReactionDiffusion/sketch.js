f4b563aeb8708373c5b198c72dbc9b30738a04e9
micuat <micuat@gmail.com>
2018-01-16 00:10:23 -0500

    13 alt

--- CC_Alt_13_ReactionDiffusion/sketch.js
+++ CC_Alt_13_ReactionDiffusion/sketch.js
@@ -18,40 +18,51 @@ var dA = 1;
 var dB = 0.5;
 var feed = 0.055;
 var k = 0.062;
+var shader;
 
 var s = function (sketch) {
 
   sketch.setup = function () {
-    sketch.createCanvas(300, 300);
+    sketch.createCanvas(600, 600);
     sketch.pixelDensity(1);
-    grid = [];
-    next = [];
-    for (var x = 0; x < sketch.width; x++) {
-      grid[x] = [];
-      next[x] = [];
-      for (var y = 0; y < sketch.height; y++) {
-        grid[x][y] = {
-          a: 1,
-          b: 0
-        };
-        next[x][y] = {
-          a: 1,
-          b: 0
-        };
-      }
-    }
+    // grid = [];
+    // next = [];
+    // for (var x = 0; x < sketch.width; x++) {
+    //   grid[x] = [];
+    //   next[x] = [];
+    //   for (var y = 0; y < sketch.height; y++) {
+    //     grid[x][y] = {
+    //       a: 1,
+    //       b: 0
+    //     };
+    //     next[x][y] = {
+    //       a: 1,
+    //       b: 0
+    //     };
+    //   }
+    // }
 
-    for (var i = 100; i < 110; i++) {
-      for (var j = 100; j < 110; j++) {
-        grid[i][j].b = 1;
-      }
-    }
+    // var d = 2;
+    // for (var i = sketch.width/2-d; i < sketch.width/2+d; i++) {
+    //   for (var j = sketch.height/2-d; j < sketch.height/2+d; j++) {
+    //     grid[i-100][j].b = 1;
+    //   }
+    // }
+    // for (var i = sketch.width/2-d; i < sketch.width/2+d; i++) {
+    //   for (var j = sketch.height/2-d; j < sketch.height/2+d; j++) {
+    //     grid[i+100][j].b = 1;
+    //   }
+    // }
+    sketch.frameRate(15);
+    shader = sketch.loadShader(sketch.sketchPath("../CC_Alt_13_ReactionDiffusion/frag.glsl"));//,
+//    sketch.sketchPath("../CC_Alt_13_ReactionDiffusion/vert.glsl"))
 
   }
 
   sketch.draw = function () {
     console.log(sketch.frameRate())
-    sketch.background(51);
+
+    // sketch.shader(shader);
 
     for (var x = 1; x < sketch.width - 1; x++) {
       for (var y = 1; y < sketch.height - 1; y++) {
@@ -76,15 +87,17 @@ var s = function (sketch) {
     if(sketch.pixels == null) return;
     for (var x = 0; x < sketch.width; x++) {
       for (var y = 0; y < sketch.height; y++) {
-        var pix = (x + y * sketch.width) * 4;
         var a = next[x][y].a;
         var b = next[x][y].b;
-        var c = sketch.floor((a - b) * 255);
-        c = sketch.constrain(c, 0, 255);
         if(sketch.isLiveJs == true) {
-          sketch.pixels[(x + y * sketch.width)] = pApplet.color(c);
+          // sketch.pixels[(x + y * sketch.width)] = pApplet.color(c);
+          sketch.pixels[(x + y * sketch.width)] = pApplet.color((a - b) * 255);
+          
         }
         else {
+          var c = sketch.floor((a - b) * 255);
+          c = sketch.constrain(c, 0, 255);
+          var pix = (x + y * sketch.width) * 4;
           sketch.pixels[pix + 0] = c;
           sketch.pixels[pix + 1] = c;
           sketch.pixels[pix + 2] = c;
@@ -94,6 +107,7 @@ var s = function (sketch) {
     }
     sketch.updatePixels();
 
+    sketch.g.filter(shader);
 
     sketch.swap();
 
@@ -137,6 +151,9 @@ var s = function (sketch) {
     next = temp;
   }
 
+  sketch.mousePressed = function () {
+    shader = sketch.loadShader(sketch.sketchPath("../CC_Alt_13_ReactionDiffusion/frag.glsl"));
+  }
 };
 
 var myp5 = new p5(s);
