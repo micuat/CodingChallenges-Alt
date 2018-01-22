d02f0a842500da103e0c8d64fbe397928cf18da6
micuat <micuat@gmail.com>
2018-01-21 19:50:22 -0500

    19 instance mode (interpret from youtube, won't work on livejs yet)

---
+++ CC_19_Superellipse/sketch.js
@@ -0,0 +1,51 @@
+// Coding Rainbow
+
+// instance mode by Naoto Hieda
+
+function sgn(val) {
+  if (val > 0)
+    return 1;
+  else if (val < 0)
+    return -1;
+  else
+    return 0;
+}
+
+var s = function (sketch) {
+
+  var slider;
+
+  sketch.setup = function () {
+    sketch.createCanvas(400, 400);
+    slider = sketch.createSlider(0, 10, 2, 0.01)
+  }
+
+  sketch.draw = function () {
+    sketch.background(51);
+    sketch.translate(sketch.width / 2, sketch.height / 2);
+
+    var a = 100;
+    var b = 100;
+    var n = slider.value();
+    // var r = 100;
+
+    sketch.stroke(255);
+    sketch.noFill();
+
+    sketch.beginShape();
+    for (var angle = 0; angle < sketch.TWO_PI; angle += 0.1) {
+      // var x = r * sketch.cos(a);
+      // var y = r * sketch.sin(a);
+
+      var na = 2 / n;
+      var x = sketch.pow(sketch.abs(sketch.cos(angle)), na) * a * sgn(sketch.cos(angle));
+      var y = sketch.pow(sketch.abs(sketch.sin(angle)), na) * b * sgn(sketch.sin(angle));
+
+      sketch.vertex(x, y);
+    }
+    sketch.endShape(sketch.CLOSE);
+  }
+
+};
+
+var myp5 = new p5(s);
