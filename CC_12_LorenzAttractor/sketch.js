7cad0b52f2c762414c7ce430138ee571e8a40a82
micuat <micuat@gmail.com>
2018-01-23 16:43:42 -0500

    12 var -> let

--- CC_12_LorenzAttractor/sketch.js
+++ CC_12_LorenzAttractor/sketch.js
@@ -1,18 +1,17 @@
 // instance mode by Naoto Hieda
 
-var x = 0.01;
-var y = 0;
-var z = 0;
-
-var a = 10;
-var b = 28;
-var c = 8.0 / 3.0;
-
-var points = new Array();
-
-
 var s = function (sketch) {
 
+  let x = 0.01;
+  let y = 0;
+  let z = 0;
+  
+  let a = 10;
+  let b = 28;
+  let c = 8.0 / 3.0;
+  
+  let points = new Array();
+  
   sketch.setup = function () {
     sketch.createCanvas(800, 600, sketch.WEBGL);
     sketch.colorMode(sketch.HSB);
@@ -21,10 +20,10 @@ var s = function (sketch) {
   sketch.draw = function () {
     sketch.background(0);
 
-    var dt = 0.01;
-    var dx = (a * (y - x)) * dt;
-    var dy = (x * (b - z) - y) * dt;
-    var dz = (x * y - c * z) * dt;
+    let dt = 0.01;
+    let dx = (a * (y - x)) * dt;
+    let dy = (x * (b - z) - y) * dt;
+    let dz = (x * y - c * z) * dt;
     x = x + dx;
     y = y + dy;
     z = z + dz;
@@ -32,19 +31,19 @@ var s = function (sketch) {
     points.push(new p5.Vector(x, y, z));
 
     sketch.translate(0, 0, -80);
-    var camX = sketch.map(sketch.mouseX, 0, sketch.width, -200, 200);
-    var camY = sketch.map(sketch.mouseY, 0, sketch.height, -200, 200);
+    let camX = sketch.map(sketch.mouseX, 0, sketch.width, -200, 200);
+    let camY = sketch.map(sketch.mouseY, 0, sketch.height, -200, 200);
     sketch.camera(camX, camY, (sketch.height / 2.0) / sketch.tan(sketch.PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
     //translate(width/2, height/2);
     sketch.scale(5);
     sketch.stroke(255);
     sketch.noFill();
 
-    var hu = 0;
+    let hu = 0;
     sketch.beginShape();
 
-    for (var i in points) {
-      var v = points[i];
+    for (let i in points) {
+      let v = points[i];
       sketch.stroke(hu, 255, 255);
       sketch.vertex(v.x, v.y, v.z);
       //PVector offset = PVector.random3D();
