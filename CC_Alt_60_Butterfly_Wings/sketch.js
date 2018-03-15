029b75aacbba6bd29819f4d0dd3c79448cba9439
micuat <micuat@gmail.com>
2018-03-15 01:07:46 -0400

    60 alt mod

--- CC_Alt_60_Butterfly_Wings/sketch.js
+++ CC_Alt_60_Butterfly_Wings/sketch.js
@@ -10,52 +10,83 @@ var yoff = 0;
 var s = function (sketch) {
   sketch.setup = function () {
     sketch.createCanvas(800, 800);
+    sketch.background(0);
   }
 
   sketch.draw = function () {
-    sketch.background(0);
+    sketch.background(0, 5);
     sketch.translate(sketch.width / 2, sketch.height / 2);
+    //rotate(PI / 2);
 
     sketch.stroke(255);
-    sketch.fill(255, 150);
+    sketch.fill(255, 10);
     sketch.strokeWeight(1);
 
-    sketch.translate(0, -100);
+    var da = sketch.PI / 300;
+    var dx = 0.05;
 
-    sketch.push();
-    sketch.scale(1, sketch.map(sketch.cos(sketch.millis() * 0.002), -1, 1, 0.5, 1));
-    sketch.rotate(0.2);
-    sketch.drawWing(10, 2, 50, 30, 100, 400);
-    sketch.rotate(-0.4);
-    sketch.scale(-1, 1);
-    sketch.drawWing(10, 2, 50, 30, 100, 400);
-    sketch.pop();
-    yoff += 0.04;
-
-    sketch.push();
-    sketch.scale(sketch.map(sketch.sin(sketch.millis() * 0.001 + sketch.PI/2), -1, 1, 0.5, 1), 1);
-    sketch.rotate(-0.1);
-    sketch.drawWing(10, 6, 20, 0, 200, 450);
-    sketch.rotate(0.2);
-    sketch.scale(-1, 1);
-    sketch.drawWing(10, 6, 20, 0, 200, 450);
-    sketch.pop();
-
-    // let da = sketch.PI / 200;
-    // let dx = 0.05;
-    // for (let a = 0; a <= sketch.TWO_PI; a += da) {
-    //   let r = 50;
-    //   let x = r * sketch.cos(a) / 4;
-    //   let y = r * sketch.sin(a);
-    //   for (let i = 0; i < 5; i++) {
-    //     let p = sketch.random(1) + i / 5;
-    //     let px = p * x;
-    //     let py = p * y + 60;
-    //     sketch.point(px, py);
+    var xoff = 0;
+    for (let i = 0; i < 3; i++) {
+      sketch.push();
+      let sc = sketch.map(i, 0, 2, 0.5, 1);
+      sketch.scale(sc, sc);
+      sketch.noStroke();
+      sketch.beginShape();
+      for (var a = 0; a <= sketch.TWO_PI; a += da) {
+        let angle = a;
+        var n = sketch.noise(xoff, yoff);
+        var r = sketch.sin(2 * a) * sketch.map(n, 0, 1, 200, 400);
+        if (0 <= a && a < sketch.PI / 2) angle += 0.2;
+        else if (sketch.PI / 2 * 3 <= a && a < sketch.PI * 2) angle -= 0.2;
+        else if (sketch.PI / 2 <= a && a < sketch.PI) { angle += 0.4; r *= 0.8; }
+        else { angle -= 0.4; r *= 0.8; }
+        var x = r * sketch.cos(angle);
+        var y = r * sketch.sin(angle);
+        if (a < sketch.PI) {
+          xoff += dx;
+        } else {
+          xoff -= dx;
+        }
+        //point(x, y);
+        sketch.vertex(x, y);
+      }
+      sketch.endShape();
+      sketch.pop();
+      yoff+=0.001;
+    }
+    // sketch.strokeWeight(2);
+    // var xoff = 0;
+    // for (var a = 0; a <= sketch.TWO_PI; a += da) {
+    //   let angle = a;
+    //   var n = sketch.noise(xoff, yoff+0.2);
+    //   var r = sketch.sin(2 * a) * sketch.map(n, 0, 1, 200, 400);
+    //   if(0 <= a && a < sketch.PI/2) angle += 0.2;
+    //   else if(sketch.PI/2*3 <= a && a < sketch.PI*2) angle -= 0.2;
+    //   else if(sketch.PI/2 <= a && a < sketch.PI) {angle += 0.4;r*=0.8;}
+    //   else {angle -= 0.4;r*=0.8;}
+    //   if (a < sketch.PI) {
+    //     xoff += dx;
+    //   } else {
+    //     xoff -= dx;
+    //   }
+    //   sketch.stroke(0);
+    //   // for(let i = 0; i < 3; i++) {
+    //   //   let rr = r + sketch.random(-3, 3);
+    //   //   var x = rr * sketch.cos(angle);
+    //   //   var y = rr * sketch.sin(angle);
+    //   //   sketch.point(x, y);
+    //   // }
+    //   sketch.stroke(0, 100);
+    //   for(let i = 0; i < 1; i++) {
+    //     let j = sketch.floor(sketch.random(4)) / 4;
+    //     let rr = r * (sketch.randomGaussian() / 40 + j);
+    //     var x = rr * sketch.cos(angle);
+    //     var y = rr * sketch.sin(angle);
+    //     sketch.point(x, y);
     //   }
     // }
 
-    yoff += 0.04;
+    yoff += 0.002;
   }
 
   sketch.drawWing = function (itr, stripe, x1, x2, f1, f2) {
@@ -77,7 +108,7 @@ var s = function (sketch) {
       } else {
         xoff -= dx;
       }
-      if(a == 0) {
+      if (a == 0) {
         for (let i = 0; i < itr; i++) {
           let p = i / itr;
           p = sketch.sqrt(p);
@@ -86,8 +117,8 @@ var s = function (sketch) {
           sketch.vertex(px, py);
         }
       }
-      else if(a >= sketch.TWO_PI-da) {
-        for (let i = itr-1; i >= 0; i--) {
+      else if (a >= sketch.TWO_PI - da) {
+        for (let i = itr - 1; i >= 0; i--) {
           let p = i / itr;
           p = sketch.sqrt(p);
           let px = p * x;
@@ -121,8 +152,8 @@ var s = function (sketch) {
       }
       for (let i = 0; i < itr; i++) {
         let p = 0;
-        if(stripe != 0) {
-          p = parseInt(i/itr*stripe)/stripe + sketch.randomGaussian()/7/stripe;//sketch.random(sketch.PI*2);
+        if (stripe != 0) {
+          p = parseInt(i / itr * stripe) / stripe + sketch.randomGaussian() / 7 / stripe;//sketch.random(sketch.PI*2);
         }
         else {
           p = sketch.random(1);
