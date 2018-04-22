9090d9945a645a0779c2db9effbeb8920960787c
micuat <micuat@gmail.com>
2018-04-22 00:12:15 -0400

    95 indent

--- CC_95_Approximating_Pi/sketch.js
+++ CC_95_Approximating_Pi/sketch.js
@@ -9,53 +9,53 @@
 
 var s = function (p) {
 
-let r = 200;
+  let r = 200;
 
-let total = 0;
-let circle = 0;
+  let total = 0;
+  let circle = 0;
 
-let recordPI = 0;
+  let recordPI = 0;
 
-p.setup = function () {
-  p.createCanvas(402, 402);
-  p.background(0);
-  p.translate(p.width/2, p.height/2);
-  p.stroke(255);
-  p.strokeWeight(4);
-  p.noFill();
-  p.ellipse(0, 0, r*2, r*2);
-  p.rectMode(p.CENTER);
-  p.rect(0, 0, r*2, r*2);
-}
+  p.setup = function () {
+    p.createCanvas(402, 402);
+    p.background(0);
+    p.translate(p.width / 2, p.height / 2);
+    p.stroke(255);
+    p.strokeWeight(4);
+    p.noFill();
+    p.ellipse(0, 0, r * 2, r * 2);
+    p.rectMode(p.CENTER);
+    p.rect(0, 0, r * 2, r * 2);
+  }
 
-p.draw = function () {
-  p.translate(p.width/2, p.height/2);
+  p.draw = function () {
+    p.translate(p.width / 2, p.height / 2);
 
-  for (let i = 0; i < 10000; i++) {
-    let x = p.random(-r, r);
-    let y = p.random(-r, r);
-    total++;
+    for (let i = 0; i < 10000; i++) {
+      let x = p.random(-r, r);
+      let y = p.random(-r, r);
+      total++;
 
-    let d = x * x + y * y;
-    if (d < r * r) {
-      circle++;
-      p.stroke(100, 255, 0, 100);
-    } else {
-      p.stroke(0, 100, 255, 100);
-    }
-    p.strokeWeight(1);
-    p.point(x, y);
+      let d = x * x + y * y;
+      if (d < r * r) {
+        circle++;
+        p.stroke(100, 255, 0, 100);
+      } else {
+        p.stroke(0, 100, 255, 100);
+      }
+      p.strokeWeight(1);
+      p.point(x, y);
 
-    let pi = 4 * (circle / total);
-    let recordDiff = Math.abs(Math.PI - recordPI);
-    let diff = Math.abs(Math.PI - pi);
-    if (diff < recordDiff) {
-      recordDiff = diff;
-      recordPI = pi;
-      print(recordPI);
+      let pi = 4 * (circle / total);
+      let recordDiff = Math.abs(Math.PI - recordPI);
+      let diff = Math.abs(Math.PI - pi);
+      if (diff < recordDiff) {
+        recordDiff = diff;
+        recordPI = pi;
+        print(recordPI);
+      }
     }
   }
-}
 
 };
 
