021569135f776d5e1a5eb2df20ab00d8eabf88f6
micuat <micuat@gmail.com>
2018-04-25 02:15:51 +0300

    97 instance only 1 page

---
+++ CC_97_Book_of_Pi_1/sketch.js
@@ -0,0 +1,58 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// Book of Pi 1
+// https://youtu.be/tbvG9hrJxOU
+
+// instance mode by Naoto Hieda
+
+var pi;
+
+var s = function (p) {
+  p.setup = function () {
+    p.createCanvas(1000, 1000, p.PDF, "../CC_97_Book_of_Pi_1/bookofpi-10million-text.pdf");
+    // let pdf = g;  // Get the renderer
+    pi = p.loadStrings("../CC_97_Book_of_Pi_1/pi-10million.txt")[0];
+
+    let cols = 100;
+    let rows = 100;
+
+    //beginDraw();
+    p.colorMode(p.HSB, 1.0);
+    let w = p.width / cols;
+    let h = p.height / rows;
+    let index = 0;
+
+    let totalPages = pi.length() / (cols * rows);
+    print(totalPages);
+
+    for (let i = 0; i < 0; i++) {
+
+      for (let y = 0; y < p.height; y += h) {
+        for (let x = 0; x < p.width; x += w) {
+          let s = "" + pi.charAt(index);
+          let digit = p.nf(s);
+          let hue = digit / 10.0;
+          p.fill(hue, 1, 1);
+          p.noStroke();
+          p.rect(x, y, w, h);
+          // fill(255-bright);
+          //textAlign(CENTER,CENTER);
+          //text(digit,x+w/2,y+h/2);
+          index++;
+          if (index >= pi.length()) {
+            index = pi.length() - 1;
+          }
+        }
+      }
+      print("Page " + i + " complete!");
+      // pdf.nextPage();
+    }
+    print("Finished");
+    p.exit();
+    //endRecord();
+  }
+};
+
+var myp5 = new p5(s);
