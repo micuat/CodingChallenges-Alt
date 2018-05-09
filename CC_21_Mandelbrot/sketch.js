140a381f34d1f48b148435fdc91a66eb0daaa1fa
micuat <micuat@gmail.com>
2018-05-09 15:40:19 +0900

    21 instance mod

--- CC_21_Mandelbrot/sketch.js
+++ CC_21_Mandelbrot/sketch.js
@@ -8,33 +8,33 @@
 var minval = -0.5;
 var maxval = 0.5;
 
-var s = function (sketch) {
-  sketch.setup = function () {
-    sketch.createCanvas(200, 200);
-    sketch.pixelDensity(1);
+var s = function (p) {
+  p.setup = function () {
+    p.createCanvas(200, 200);
+    p.pixelDensity(1);
   }
 
-  sketch.draw = function () {
-    var maxiterations = 100;
+  p.draw = function () {
+    let maxiterations = 100;
 
-    sketch.loadPixels();
-    if(sketch.pixels == null) return;
-    for (var x = 0; x < sketch.width; x++) {
-      for (var y = 0; y < sketch.height; y++) {
+    p.loadPixels();
+    if(p.pixels == null) return;
+    for (let x = 0; x < p.width; x++) {
+      for (let y = 0; y < p.height; y++) {
 
-        var minValue = sketch.map(sketch.mouseX, 0, sketch.width, -2.5, 0);
-        var maxValue = sketch.map(sketch.mouseY, 0, sketch.height, 0, 2.5);
-        var a = sketch.map(x, 0, sketch.width, minValue, maxValue);
-        var b = sketch.map(y, 0, sketch.height, minValue, maxValue);
+        let minValue = p.map(p.mouseX, 0, p.width, -2.5, 0);
+        let maxValue = p.map(p.mouseY, 0, p.height, 0, 2.5);
+        let a = p.map(x, 0, p.width, minValue, maxValue);
+        let b = p.map(y, 0, p.height, minValue, maxValue);
 
-        var ca = a;
-        var cb = b;
+        let ca = a;
+        let cb = b;
 
-        var n = 0;
+        let n = 0;
 
         while (n < maxiterations) {
-          var aa = a * a - b * b;
-          var bb = 2 * a * b;
+          let aa = a * a - b * b;
+          let bb = 2 * a * b;
           a = aa + ca;
           b = bb + cb;
           if (a * a + b * b > 16) {
@@ -43,28 +43,28 @@ var s = function (sketch) {
           n++;
         }
 
-        var bright = sketch.map(n, 0, maxiterations, 0, 1);
-        bright = sketch.map(sketch.sqrt(bright), 0, 1, 0, 255);
+        let bright = p.map(n, 0, maxiterations, 0, 1);
+        bright = p.map(p.sqrt(bright), 0, 1, 0, 255);
 
         if (n == maxiterations) {
           bright = 0;
         }
 
-        if(sketch.isLiveJs == true) {
-          sketch.pixels[(x + y * sketch.width)] = pApplet.color(bright);
+        if(p.isLiveJs == true) {
+          p.pixels[(x + y * p.width)] = pApplet.color(bright);
         }
         else {
-          var pix = (x + y * sketch.width) * 4;
-          sketch.pixels[pix + 0] = bright;
-          sketch.pixels[pix + 1] = bright;
-          sketch.pixels[pix + 2] = bright;
-          sketch.pixels[pix + 3] = 255;
+          let pix = (x + y * p.width) * 4;
+          p.pixels[pix + 0] = bright;
+          p.pixels[pix + 1] = bright;
+          p.pixels[pix + 2] = bright;
+          p.pixels[pix + 3] = 255;
         }
       }
     }
-    sketch.updatePixels();
+    p.updatePixels();
   }
 
 };
 
-var myp5 = new p5(s);
+var p021 = new p5(s);
