6bbe7c4ab724e73cebfe6d1779b1f293642a5402
micuat <micuat@gmail.com>
2018-05-12 12:16:35 +0900

    30 instance mod

--- CC_30_phyllotaxis/sketch.js
+++ CC_30_phyllotaxis/sketch.js
@@ -12,30 +12,30 @@ var points = [];
 
 var start = 0;
 
-var s = function (sketch) {
+var s = function (p) {
 
-  sketch.setup = function () {
-    sketch.createCanvas(400, 400);
-    // sketch.angleMode(sketch.DEGREES);
-    sketch.colorMode(sketch.HSB);
+  p.setup = function () {
+    p.createCanvas(400, 400);
+    // p.angleMode(p.DEGREES);
+    p.colorMode(p.HSB);
   }
 
-  sketch.draw = function () {
-    sketch.background(0);
-    sketch.translate(sketch.width / 2, sketch.height / 2);
-    sketch.rotate(n * 0.3);
-    for (var i = 0; i < n; i++) {
-      var a = i * sketch.radians(137.5);
-      var r = c * sketch.sqrt(i);
-      var x = r * sketch.cos(a);
-      var y = r * sketch.sin(a);
-      // var hu = sketch.sin(start + i * 0.5);
-      // hu = sketch.map(hu, -1, 1, 0, 360);
-      var hu = i+start;
+  p.draw = function () {
+    p.background(0);
+    p.translate(p.width / 2, p.height / 2);
+    p.rotate(n * 0.3);
+    for (let i = 0; i < n; i++) {
+      let a = i * p.radians(137.5);
+      let r = c * p.sqrt(i);
+      let x = r * p.cos(a);
+      let y = r * p.sin(a);
+      // var hu = p.sin(start + i * 0.5);
+      // hu = p.map(hu, -1, 1, 0, 360);
+      let hu = i+start;
       hu = i/3.0 % 360;
-      sketch.fill(hu, 255, 255);
-      sketch.noStroke();
-      sketch.ellipse(x, y, 4, 4);
+      p.fill(hu, 255, 255);
+      p.noStroke();
+      p.ellipse(x, y, 4, 4);
     }
     n += 5;
     start += 5;
@@ -43,4 +43,4 @@ var s = function (sketch) {
 
 };
 
-var myp5 = new p5(s);
+var p030 = new p5(s);
