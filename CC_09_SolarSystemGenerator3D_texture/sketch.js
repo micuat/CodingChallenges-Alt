7c1b6c81c2065d1ceefa6d77f8d685a3f0741026
micuat <micuat@gmail.com>
2018-05-19 11:18:26 +0900

    09 instance mod

--- CC_09_SolarSystemGenerator3D_texture/sketch.js
+++ CC_09_SolarSystemGenerator3D_texture/sketch.js
@@ -7,31 +7,31 @@
 
 var textures = [];
 
-var s = function (sketch) {
+var s = function (p) {
 
   var sun;
   var cam;
   var sunTexture;
 
-  sketch.setup = function () {
-    sketch.createCanvas(600, 600);
-    sunTexture = sketch.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/sun.jpg");
-    textures[0] = sketch.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/mars.jpg");
-    textures[1] = sketch.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/earth.jpg");
-    textures[2] = sketch.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/mercury.jpg");
+  p.setup = function () {
+    p.createCanvas(600, 600);
+    sunTexture = p.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/sun.jpg");
+    textures[0] = p.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/mars.jpg");
+    textures[1] = p.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/earth.jpg");
+    textures[2] = p.loadImage("../../CC_09_SolarSystemGenerator3D_texture/data/mercury.jpg");
     cam = new Packages.peasy.PeasyCam(pApplet.that, 500);
-    sun = new Planet(sketch, 50, 0, 0, sunTexture);
+    sun = new Planet(50, 0, 0, sunTexture);
     sun.spawnMoons(4, 1);
   }
 
-  sketch.draw = function () {
-    sketch.background(0);
-    sketch.ambientLight(255,255,255);
-    sketch.pointLight(255, 255, 255, 0, 0, 0);
+  p.draw = function () {
+    p.background(0);
+    p.ambientLight(255,255,255);
+    p.pointLight(255, 255, 255, 0, 0, 0);
     sun.show();
     sun.orbit();
     }
 
 };
 
-var myp5 = new p5(s);
+var p009 = new p5(s);
